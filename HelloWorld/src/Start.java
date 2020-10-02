013f5c2ddd7d872c75c24f438e948f63306a14fc
Akash Raj <43521630+akashrajmuj@users.noreply.github.com>
2020-10-02 15:14:25 +0530

    Added calling using Object

--- HelloWorld/src/Start.java
+++ HelloWorld/src/Start.java
@@ -1,9 +1,16 @@
 
-public class Start {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Start {  // a simple program to demonstrate function calling using object 
+	String str;
+	
+	public static void main(String[] args) 
+		{
+		Start obj=new Start(); 	//object of class Start
+		System.out.println( obj.hello() ); //call hello method using object 
+		}
+	public String hello()
+	{
+		str="Hello World";  
+		return(str);
 	}
-
+	
 }
